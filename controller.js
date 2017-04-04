ecdfaf80fc920358745e0c31881391bc1da4c6ff
dawn_scroll <dibyomails007gmail.com>
2017-04-05 00:48:19 +0530

    json-prb

--- controller.js
+++ controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('mainApp', []);
 
 app.controller('people', function($scope, $http){
-	$http.get('http://127.0.0.1/Angular-Learn/database.json');
+	$http.get('http://127.0.0.1:8006/Angular-Learn/database.json')
+	.then( function(response) {
+		
+		var data = response.records;
+		$scope.persons = data;
+		console.log(data);
+	});
 }); 
